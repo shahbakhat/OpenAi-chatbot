c9f6d2142ec423e255fb6b638615dc2ad9e0950d
Shah Bakhat <shahbakhat008@gmail.com>
2023-03-14 17:00:16 +0000

    created repsonse

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
-import openai
 import os
+import openai
+
+API_KEY = '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'
+openai.api_key = API_KEY
+
+prompt = input()
+
+model = 'text-davinci-003'
+response = openai.Completion.create(
+    prompt= prompt,
+    model= model,
+    max_tokens= 1500,
+    temperature= 0.8,
+    n = 1,
+    stop = ['---'],
+)
+# print(response)
+#  "created": 1678810576,
+# "id": "cmpl-6u1bs8IUlYdc3UudkvAavvt8pVAty",
+# "model": "text-davinci-003",
+# "object": "text_completion",
+# "usage": {
+#  "completion_tokens": 16,
+#   "prompt_tokens": 6,
+#   "total_tokens": 22
+print(response.usage.total_tokens)
+
+for results in response.choices[0].text:
+    print(results.text)
